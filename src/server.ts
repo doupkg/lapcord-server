59131982040c7e11a38b4a51c63b213d3d04138f
Jesus Alejandro <jesusmolinarodrigue26@gmail.com>
2023-03-30 19:07:33 -0300

    fix: prettier/eslint errors
    ```  18:5  error  'connection' is never reassigned. Use 'const' instead  prefer-const
      20:5  error  'documents' is never reassigned. Use 'const' instead   prefer-const
      21:5  error  'rpc' is never reassigned. Use 'const' instead         prefer-const
      44:7  error  'wdps' is never reassigned. Use 'const' instead        prefer-const```

--- src/server.ts
+++ src/server.ts
@@ -1,65 +1,68 @@
 #! /usr/bin/env node
 
 import {
-    createConnection,
-    ProposedFeatures,
-    TextDocumentSyncKind,
-    InitializeResult,
-    ShowMessageNotification,
-    ShowMessageParams,
-    MessageType,
-    TextDocuments,
-} from "vscode-languageserver/node"
+  createConnection,
+  ProposedFeatures,
+  TextDocumentSyncKind,
+  InitializeResult,
+  ShowMessageNotification,
+  ShowMessageParams,
+  MessageType,
+  TextDocuments,
+} from 'vscode-languageserver/node'
 
-import { TextDocument } from "vscode-languageserver-textdocument"
+import { TextDocument } from 'vscode-languageserver-textdocument'
 
-import { Client } from "@xhayper/discord-rpc"
+import { Client } from '@xhayper/discord-rpc'
 
-let connection = createConnection(ProposedFeatures.all)
+const connection = createConnection(ProposedFeatures.all)
+const documents: TextDocuments<TextDocument> = new TextDocuments(TextDocument)
 
-let documents: TextDocuments<TextDocument> = new TextDocuments(TextDocument);
-let rpc = new Client({ clientId: "1084261309677318154", transport: { type: "ipc" } })
+const rpc = new Client({
+  clientId: '1084261309677318154',
+  transport: { type: 'ipc' },
+})
 
 connection.onInitialize(() => {
-    connection.console.log("Lapcord Initialialized!")
-    const result: InitializeResult = {
-        capabilities: {
-            textDocumentSync: {
-                openClose: true,
-                change: TextDocumentSyncKind.Incremental,
-            },
-        },
-        serverInfo: {
-            name: "Lapcord",
-            version: "0.0.1"
-        }
-    };
-    return result;
+  connection.console.log('Lapcord Initialialized!')
+  const result: InitializeResult = {
+    capabilities: {
+      textDocumentSync: {
+        openClose: true,
+        change: TextDocumentSyncKind.Incremental,
+      },
+    },
+    serverInfo: {
+      name: 'Lapcord',
+      version: '0.0.1',
+    },
+  }
+  return result
 })
 
 connection.onInitialized(async () => {
-    let wdps = await connection.window.createWorkDoneProgress()
-    wdps.begin("Discord Presence", 0, "Connecting...")
-    await rpc.login().catch((r) => {
-        wdps.report("Error")
-        connection.sendNotification(ShowMessageNotification.method, {
-            message: `Discord RPC Error: ${r}`,
-            type: MessageType.Info,
-        } as ShowMessageParams)
-        setTimeout(() => wdps.done(), 10_000)
-    })
-    // rpc.on("ready", () => {
-    //     connection.sendNotification(ShowMessageNotification.method, {
-    //         message: `Discord Presence Ready`,
-    //         type: MessageType.Info,
-    //     } as ShowMessageParams)
-    // })
-    // rpc.on("disconnected", () => {
-    //     wdps.report("Disconnected")
-    // })
-    // rpc.on("connected", () => {
-    //     wdps.report("Connected")
-    // })
+  const wdps = await connection.window.createWorkDoneProgress()
+  wdps.begin('Discord Presence', 0, 'Connecting...')
+  await rpc.login().catch((r) => {
+    wdps.report('Error')
+    connection.sendNotification(ShowMessageNotification.method, {
+      message: `Discord RPC Error: ${r}`,
+      type: MessageType.Info,
+    } as ShowMessageParams)
+    setTimeout(() => wdps.done(), 10_000)
+  })
+  // rpc.on("ready", () => {
+  //     connection.sendNotification(ShowMessageNotification.method, {
+  //         message: `Discord Presence Ready`,
+  //         type: MessageType.Info,
+  //     } as ShowMessageParams)
+  // })
+  // rpc.on("disconnected", () => {
+  //     wdps.report("Disconnected")
+  // })
+  // rpc.on("connected", () => {
+  //     wdps.report("Connected")
+  // })
 })
 
 // documents.onDidOpen((params) => {
