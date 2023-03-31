8ee7420874c12991cd65e61cec29110c1e1d26ea
DexSlender <eduarzamora340@gmail.com>
2023-03-31 11:52:32 -0600

    feat(): * Add presence idle * Change tsconfig module to commonjs

--- src/server.ts
+++ src/server.ts
@@ -15,13 +15,32 @@ import { TextDocument } from 'vscode-languageserver-textdocument'
 
 import { Client } from '@xhayper/discord-rpc'
 
-const connection = createConnection(ProposedFeatures.all)
-const documents: TextDocuments<TextDocument> = new TextDocuments(TextDocument)
+let connection = createConnection(ProposedFeatures.all)
 
-const rpc = new Client({
+process.on("unhandledRejection", (r) => {
+  connection.sendNotification(ShowMessageNotification.method, {
+    message: `Discord RPC Error: ${r}`,
+    type: MessageType.Info,
+  } as ShowMessageParams)
+  // wdps.report("Error")
+  // setTimeout(() => {
+  //     wdps.done()
+  // })
+})
+
+let documents: TextDocuments<TextDocument> = new TextDocuments(TextDocument)
+let rpc = new Client({
   clientId: '1084261309677318154',
   transport: { type: 'ipc' },
 })
+
+enum IMAGE_KEYS {
+  icon_dark = "logo-dark",
+  icon = "logo-colorful",
+  idle = "idle",
+  moon = "moon",
+  file = "file"
+}
 
 connection.onInitialize(() => {
   connection.console.log('Lapcord Initialialized!')
@@ -40,66 +59,70 @@ connection.onInitialize(() => {
   return result
 })
 
+let rpc_connected: boolean = false
+
 connection.onInitialized(async () => {
   const wdps = await connection.window.createWorkDoneProgress()
   wdps.begin('Discord Presence', 0, 'Connecting...')
-  await rpc.login().catch((r) => {
-    wdps.report('Error')
+
+  rpc.on("connected", () => {
+    rpc_connected = true;
+    wdps.report("Connected")
+    setTimeout(() => {
+      wdps.report(`${rpc.user?.tag}`)
+      setTimeout(() => wdps.done(), 10_000)
+    }, 10_000)
+    rpc.user?.setActivity({
+      largeImageKey: IMAGE_KEYS.icon_dark,
+      smallImageKey: IMAGE_KEYS.idle,
+      // smallImageText: "",
+      // largeImageText: "",
+      details: "Idle",
+      // state: "",
+      startTimestamp: Date.now()
+    });
+  })
+
+  rpc.login().catch(r => {
+    wdps.report("Error")
     connection.sendNotification(ShowMessageNotification.method, {
       message: `Discord RPC Error: ${r}`,
       type: MessageType.Info,
     } as ShowMessageParams)
-    setTimeout(() => wdps.done(), 10_000)
-  })
-  // rpc.on("ready", () => {
-  //     connection.sendNotification(ShowMessageNotification.method, {
-  //         message: `Discord Presence Ready`,
-  //         type: MessageType.Info,
-  //     } as ShowMessageParams)
-  // })
-  // rpc.on("disconnected", () => {
-  //     wdps.report("Disconnected")
-  // })
-  // rpc.on("connected", () => {
-  //     wdps.report("Connected")
-  // })
+    setTimeout(() => {
+      wdps.done()
+    }, 10_000)
+  });
 })
 
-// documents.onDidOpen((params) => {
-//     connection.sendNotification(ShowMessageNotification.method, {
-//         message: `Document opened ${params.document.uri}`,
-//         type: MessageType.Info,
-//     } as ShowMessageParams)
-// })
-
-// documents.onDidClose(() => {
-//     connection.sendNotification(ShowMessageNotification.method, {
-//         message: "Document closed!",
-//         type: MessageType.Info,
-//     } as ShowMessageParams)
-// })
-
-// connection.onDidOpenTextDocument((params: DidOpenTextDocumentParams) => {
-//     // connection.console.log("Document opened!")
-//     connection.sendNotification(ShowMessageNotification.method, {
-//         message: `Document opened ${params.textDocument.uri}`,
-//         type: MessageType.Info,
-//     } as ShowMessageParams)
+// documents.onDidOpen(({ document }) => {
+//     if (!rpc_connected) return;
+//     rpc.user?.setActivity({
+//         largeImageKey: IMAGE_KEYS.file,
+//         largeImageText: `Editing a ${document.languageId.toUpperCase()} file`,
+//         smallImageKey: IMAGE_KEYS.icon,
+//         smallImageText: "Lapce",
+//         details: `Editing ${document.uri.split("/").at(-1)}`,
+//         startTimestamp: Date.now()
+//     });
 // });
 
-// connection.onDidChangeWatchedFiles(() => {
-//     connection.sendNotification(ShowMessageNotification.method, {
-//         message: "Document changed!",
-//         type: MessageType.Info,
-//     } as ShowMessageParams)
-// })
+// documents.onDidClose(() => {
+//     if (!rpc_connected) return;
+//     rpc.user?.setActivity({
+//         largeImageKey: IMAGE_KEYS.icon_dark,
+//         smallImageKey: IMAGE_KEYS.idle,
+//         // smallImageText: "",
+//         // largeImageText: "",
+//         details: "Idle",
+//         // state: "",
+//         startTimestamp: Date.now()
+//     });
+// });
 
-// connection.onDidCloseTextDocument(() => {
-//     connection.sendNotification(ShowMessageNotification.method, {
-//         message: "Document closed!",
-//         type: MessageType.Info,
-//     } as ShowMessageParams)
-// })
+connection.onExit(() => {
+  rpc.destroy()
+})
 
 documents.listen(connection)
 connection.listen()
