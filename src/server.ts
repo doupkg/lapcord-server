af4bf38cf7fcffd568ac801bb9a39ba08d0eca79
Paulo <undevoted@proton.me>
2023-04-04 09:43:24 -0500

    refactor: Code improvement

--- src/server.ts
+++ src/server.ts
@@ -1,145 +1,129 @@
 #! /usr/bin/env node
-
-import {
-  createConnection,
-  ProposedFeatures,
-  TextDocumentSyncKind,
-  InitializeResult,
-  ShowMessageNotification,
-  ShowMessageParams,
-  MessageType,
-  TextDocuments,
-} from 'vscode-languageserver/node'
-
+import { createConnection, MessageType, ProposedFeatures, ShowMessageNotification, ShowMessageParams, TextDocuments, TextDocumentSyncKind, WorkDoneProgressReporter } from 'vscode-languageserver/node'
 import { TextDocument } from 'vscode-languageserver-textdocument'
-
-import { Client } from '@xhayper/discord-rpc'
+import { Client, SetActivity } from '@xhayper/discord-rpc'
+import { existsSync } from 'node:fs'
+import { writeFile, unlink } from 'node:fs/promises'
+import * as path from 'node:path'
+import * as os from 'node:os'
 
 const connection = createConnection(ProposedFeatures.all)
+const documents = new TextDocuments(TextDocument)
 
-process.on('unhandledRejection', (r) => {
-  connection.sendNotification(ShowMessageNotification.method, {
-    message: `Discord RPC Error: ${r}`,
-    type: MessageType.Info,
-  } as ShowMessageParams)
-  // wdps.report("Error")
-  // setTimeout(() => {
-  //     wdps.done()
-  // })
-})
-
-const documents: TextDocuments<TextDocument> = new TextDocuments(TextDocument)
-const rpc = new Client({
+const client = new Client({
   clientId: '1084261309677318154',
-  transport: { type: 'ipc' },
+  transport: {
+    type: 'ipc'
+  }
 })
+
+const lockFile = path.join(os.tmpdir(), 'discord-rpc.lock')
+const CurrentTimestamp = Date.now()
+let wdps: WorkDoneProgressReporter
+let RPCConection = false
+let timerId: NodeJS.Timeout | null = null
 
 enum IMAGE_KEYS {
-  icon_dark = 'logo-dark',
-  icon = 'logo-app',
-  idle = 'idle',
-  moon = 'moon',
-  file = 'file',
+  logo = 'logo-app',
+  idle = 'idle'
 }
 
+type Type = 'idle' | 'editing'
+
+async function setActivity(type: Type, document?: TextDocument): Promise<void> {
+  if (type === 'editing' && !document) throw new Error('You need document when use Editing type!')
+  if (document && !validUri(document?.uri)) throw new TypeError('Invalid uri!')
+
+  const activityObject: SetActivity = {
+    state: type === 'idle' ? 'Idling' : 'Editing ' + document?.uri.split('/').at(-1),
+    largeImageKey: type === 'idle' ? IMAGE_KEYS.logo : fetchIcon(document?.languageId as string),
+    smallImageKey: type === 'idle' ? IMAGE_KEYS.idle : IMAGE_KEYS.logo,
+    startTimestamp: CurrentTimestamp
+  }
+
+  if (type === 'editing') {
+    activityObject.details = 'In ' + path.dirname(document?.uri.replace('file://', '') as string)
+  }
+
+  client.user?.setActivity(activityObject)
+}
+
+function startTimer(document: TextDocument, time?: number) {
+  if (timerId) clearTimeout(timerId)
+  timerId = setTimeout(async () => {
+    if (documents.get(document.uri)) setActivity('idle')
+    if (existsSync(lockFile)) unlink(lockFile)
+  }, time || 10000)
+}
+
+async function setUp() {
+  wdps = await connection.window.createWorkDoneProgress()
+  wdps.begin('Discord Presence', 0, 'Connecting...')
+
+  const lock = await createLock()
+  if (!lock) return wdps.report('Already connected!')
+
+  client.on('connected', () => {
+    RPCConection = true
+    wdps.report(client.user?.tag as string)
+    if (client.user) setActivity('idle')
+  })
+
+  client.login()
+}
+
+async function createLock(): Promise<boolean> {
+  if (existsSync(lockFile)) return false
+  await writeFile(lockFile, process.pid.toString())
+  return true
+}
+
+function validUri(uri: string) {
+  return /^file:\/\/\/.*\.[^.]+$/.test(uri)
+}
+
+function fetchIcon(languageId: string) {
+  const knownLanguages = ['javascript', 'typescript', 'rust']
+  return knownLanguages.find((x) => x === languageId) ? languageId : 'file'
+}
+
+documents.onDidSave(({ document }) => {
+  if (!RPCConection) return null
+  setActivity('editing', document)
+  startTimer(document)
+})
+
+documents.onDidChangeContent(({ document }) => {
+  if (!RPCConection) return null
+  setActivity('editing', document)
+  startTimer(document)
+})
+
+connection.onInitialized(() => setUp())
+
 connection.onInitialize(() => {
-  connection.console.log('Lapcord Initialialized!')
-  const result: InitializeResult = {
+  documents.listen(connection)
+
+  return {
     capabilities: {
       textDocumentSync: {
         openClose: true,
-        change: TextDocumentSyncKind.Incremental,
-      },
+        change: TextDocumentSyncKind.Incremental
+      }
     },
     serverInfo: {
       name: 'Lapcord',
-      version: '0.0.1',
-    },
+      version: '6.6.6'
+    }
   }
-  return result
 })
 
-let rpc_connected = false
-
-connection.onInitialized(async () => {
-  const wdps = await connection.window.createWorkDoneProgress()
-  wdps.begin('Discord Presence', 0, 'Connecting...')
-
-  rpc.on('connected', () => {
-    rpc_connected = true
-    wdps.report('Connected')
-    setTimeout(() => {
-      wdps.report(`${rpc.user?.tag}`)
-      setTimeout(() => wdps.done(), 10_000)
-    }, 10_000)
-    rpc.user?.setActivity({
-      largeImageKey: IMAGE_KEYS.icon_dark,
-      smallImageKey: IMAGE_KEYS.idle,
-      // smallImageText: "",
-      // largeImageText: "",
-      details: 'Idle',
-      // state: "",
-      startTimestamp: Date.now(),
-    })
-  })
-
-  rpc.login().catch((r) => {
-    wdps.report('Error')
-    connection.sendNotification(ShowMessageNotification.method, {
-      message: `Discord RPC Error: ${r}`,
-      type: MessageType.Info,
-    } as ShowMessageParams)
-    setTimeout(() => {
-      wdps.done()
-    }, 10_000)
-  })
-})
-
-//Dex puto no comentes con //
-//solo con /* */
-
-documents.onDidOpen(({ document }) => {
-  if (!rpc_connected) return
-
+process.on('unhandledRejection', (e) => {
   connection.sendNotification(ShowMessageNotification.method, {
-    message: `${JSON.stringify(document)}`,
-    type: MessageType.Info,
+    message: 'Error: ' + e,
+    type: MessageType.Error
   } as ShowMessageParams)
-
-  rpc.user?.setActivity({
-    largeImageKey: icon(document.languageId),
-    largeImageText: `Editing a ${document.languageId.toUpperCase()} file`,
-    smallImageKey: IMAGE_KEYS.icon,
-    smallImageText: 'Lapce',
-    details: `Editing ${document.uri.split('/').at(-1)}`,
-    startTimestamp: Date.now(),
-  })
+  wdps.report('Error')
 })
 
-// documents.onDidClose(() => {
-//     if (!rpc_connected) return;
-//     rpc.user?.setActivity({
-//         largeImageKey: IMAGE_KEYS.icon_dark,
-//         smallImageKey: IMAGE_KEYS.idle,
-//         // smallImageText: "",
-//         // largeImageText: "",
-//         details: "Idle",
-//         // state: "",
-//         startTimestamp: Date.now()
-//     });
-// });
-
-connection.onExit(() => {
-  rpc.destroy()
-})
-
-documents.listen(connection)
 connection.listen()
-
-function icon(extension: string) {
-  const knowLanguages = ['javascript', 'rust', 'typescript']
-
-  const fIcon = knowLanguages.find((e) => e === extension)
-
-  return typeof fIcon === 'string' ? fIcon : 'file'
-}
